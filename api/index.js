39a3b6394c56eca54a816e5ee9bc05043b551db9
Andrei SILIN <andrei.silin@studi.fr>
2024-06-07 15:53:18 +0200

    test

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const { Resend } = require('resend');
+//const { Resend } = require('resend');
 
 const app = express();
 const port = 3001; // Or any port of your choice
