d4617649fab0198df0a892843db40271dbcf35ab
Andrei SILIN <andrei.silin@studi.fr>
2024-06-20 19:29:53 +0200

    add hero mobile image

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -3,10 +3,49 @@ import s from "./Main/Main.module.css";
 import useViewportHeight from "../hooks/useViewportHeight";
 import "animate.css/animate.compat.css"
 import cathedrale from "../images/cathedrale_tropea.jpeg";
+import cathedraleMobile from "../images/cathedrale-mobile.png";
 import {scrollToRef} from "./Header";
+import {useMediaQuery} from "@mui/material";
 
 const Hero = ({refRooms}) => {
     const fullHeight = useViewportHeight() + 'px'
+    const isDesktop = useMediaQuery('(min-width:780px)');
+
+    if(isDesktop) {
+        return (
+            <div className={s.main} style={{height: fullHeight, transition: '.1s all ease'}}>
+                <div className={s.title}>Duomo Tropea</div>
+                <div className={s.description}>Hotel nel cuore storico di Tropea</div>
+                <div style={{
+                    width: '100%',
+                    height: '100%',
+                    borderRadius: '0 0 30px 30px',
+                    overflow: 'hidden',
+                    position: 'relative',
+                    border: '1px solid black'
+                }}>
+                    <img
+                        src={cathedrale}
+                        style={{
+                            width: '100%',
+                            height: '100%',
+                            objectFit: 'cover',
+                        }}
+                    />
+                </div>
+                <div className="button"
+                     onClick={() => scrollToRef(refRooms)}
+                     style={{
+                         position: 'absolute',
+                         bottom: '15%',
+                         left: '50%',
+                         transform: 'translate(-50%, 0)',
+                     }}>Vedi disponibilità
+                </div>
+            </div>
+
+        )
+    }
 
     return (
         <div className={s.main} style={{height: fullHeight, transition: '.1s all ease'}}>
@@ -21,7 +60,7 @@ const Hero = ({refRooms}) => {
                 border: '1px solid black'
             }}>
                 <img
-                    src={cathedrale}
+                    src={cathedraleMobile}
                     style={{
                         width: '100%',
                         height: '100%',
